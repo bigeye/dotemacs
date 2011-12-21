312bc05cf4cef0266974d879720eec4a03571336
Donghyun Cho <bigeyeguy@gmail.com>
2011-12-21 12:54:42 +0900

    Disable rcirc and Enable erc.

--- myEmacs.el
+++ myEmacs.el
@@ -246,16 +246,36 @@ and puts spaces between the elements."
 ;;; Rcirc Settings ( IRC ) ;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(require 'rcirc)
-(require 'rcirc-pounce)
-(require 'rcirc-extension)
-(setq rcirc-default-username "webirc-888")
-(setq rcirc-default-full-name "webirc-888")
-(setq rcirc-default-nick "webirc-888")
-(setq rcirc-server-alist '(("irc.hanirc.org" :port 8080)))
-(set-rcirc-decode-coding-system 'euc-kr)
-(set-rcirc-encode-coding-system 'euc-kr)
+;; (require 'rcirc)
+;; (require 'rcirc-pounce)
+;; (require 'rcirc-extension)
+;; (setq rcirc-default-username "webirc-888")
+;; (setq rcirc-default-full-name "webirc-888")
+;; (setq rcirc-default-nick "webirc-888")
+;; (setq rcirc-server-alist '(("irc.hanirc.org" :port 8080)))
+;; (set-rcirc-decode-coding-system 'euc-kr)
+;; (set-rcirc-encode-coding-system 'euc-kr)
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; ERC Settings ( IRC ) ;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(erc-autojoin-mode t)
+(setq erc-mode-line-format "%S %t")
+(and
+ (require 'erc-highlight-nicknames)
+ (add-to-list 'erc-modules 'highlight-nicknames)
+ (erc-update-modules))
+
+;; check channels
+;; (erc-track-mode t)
+;; (setq erc-track-exclude-types '("JOIN" "NICK" "PART" "QUIT" "MODE"
+
+;;                                  "324" "329" "332" "333" "353" "477"))
+;; don't show any of this
+;; (setq erc-hide-list '("JOIN" "PART" "QUIT" "NICK"))
+
+(load "~/.erc_accounts" t)
 
 ;;;;;;;;;;;;;;;;;;;;
 ;;; GUD Settings ;;;
