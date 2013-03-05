605af11b1ecc9f52ca36d0b97b6c07eea1967c8b
Donghyun Cho <bigeyeguy@gmail.com>
2013-03-05 22:33:15 +0900

    Set new-line-and-indent as true

--- bigeye/bindings.el
+++ bigeye/bindings.el
@@ -1,6 +1,7 @@
 (define-key ctl-x-map "\C-m" jabber-global-keymap)
 (global-set-key [Hangul] 'toggle-input-method)
 (global-set-key (kbd "<M-SPC>") 'toggle-input-method)
+(global-set-key (kbd "<RET>") 'newline-and-indent)
 
 (global-set-key (kbd "C-x C-j C-o") 'browse-url-at-point)
 (global-set-key (kbd "C-x C-j o") 'browse-url)
