6de2dec0c4ee652ba0b8ff08c58f7a91c45e37b1
Donghyun Cho <bigeyeguy@gmail.com>
2013-06-06 02:12:35 +0900

    Set up org-crypt

--- bigeye/org-mode.el
+++ bigeye/org-mode.el
@@ -35,4 +35,22 @@
 
 	      (setq org-refile-targets '((nil :maxlevel . 2)
 					 (org-agenda-files :maxlevel . 1)))
+
+              (require 'org-crypt)
+              (org-crypt-use-before-save-magic)
+              (setq org-tags-exclude-from-inheritance (quote ("crypt")))
+
+              (setq org-crypt-key nil)
+              ;; GPG key to use for encryption
+              ;; Either the Key ID or set to nil to use symmetric encryption.
+
+              (setq auto-save-default nil)
+              ;; Auto-saving does not cooperate with org-crypt.el: so you need
+              ;; to turn it off if you plan to use org-crypt.el quite often.
+              ;; Otherwise, you'll get an (annoying) message each time you
+              ;; start Org.
+
+              ;; To turn it off only locally, you can insert this:
+              ;;
+              ;; # -*- buffer-auto-save-file-name: nil; -*-
 )
