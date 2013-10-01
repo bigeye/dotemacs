7025f8283b12de141e65ed2268e8aaa68d6a9a93
Donghyun Cho <bigeyeguy@gmail.com>
2013-10-01 17:11:42 +0900

    Mobile org auto sync extract custom variables

--- bigeye/org-mode.el
+++ bigeye/org-mode.el
@@ -1,56 +1,56 @@
 (require 'org)
+
+;;; This code snippet is imported from
+;;; http://stackoverflow.com/a/8432149/776937.
+;;; Originally authored by Chris Zheng
+
+(defvar org-mobile-sync-timer nil)
+
+(defvar org-mobile-sync-idle-secs (* 60 60))
+
+(defun org-mobile-sync ()
+  (interactive)
+  (message "org-mobile-sync")
+  (org-mobile-pull)
+  (org-mobile-push))
+
+(defun org-mobile-sync-enable ()
+  "enable mobile org idle sync"
+  (interactiv)
+  (setq org-mobile-sync-timer
+        (run-with-idle-timer org-mobile-sync-idle-secs t
+                             'org-mobile-sync)))
+
+(defun org-mobile-sync-disable ()
+  "disable mobile org idle sync"
+  (interactive)
+  (cancel-timer org-mobile-sync-timer))
+
+;;; Org-mode config
+
 (stante-after org
-	      ;; org-mode
-	      (setq org-log-done 'time)
-	      (setq org-agenda-files (list "~/org/emacs.org"
-					   "~/org/blog.org"))
-	      ;; (set-face-underline 'org-level-1 nil)
-	      (define-key global-map "\C-ca" 'org-agenda)
+  ;; org-mode
+  ;; (set-face-underline 'org-level-1 nil)
+  (define-key global-map "\C-ca" 'org-agenda)
 
-	      (setq org-default-notes-file "~/org/notes.org")
+  (add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
+  (add-to-list 'auto-mode-alist '("\\.org.gpg$" . org-mode))
+  (add-to-list 'auto-mode-alist '("\\.ref$" . org-mode))
+  (add-to-list 'auto-mode-alist '("\\.ref.gpg$" . org-mode))
+  (add-to-list 'auto-mode-alist '("\\.notes$" . org-mode))
 
-	      (add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
-	      (add-to-list 'auto-mode-alist '("\\.org.gpg$" . org-mode))
-	      (add-to-list 'auto-mode-alist '("\\.ref$" . org-mode))
-	      (add-to-list 'auto-mode-alist '("\\.ref.gpg$" . org-mode))
-	      (add-to-list 'auto-mode-alist '("\\.notes$" . org-mode))
+  (global-set-key "\C-cl" 'org-store-link)
+  (global-set-key "\C-cc" 'org-capture)
+  (global-set-key "\C-ca" 'org-agenda)
+  (global-set-key "\C-cb" 'org-iswitchb)
 
-	      (global-set-key "\C-cl" 'org-store-link)
-	      (global-set-key "\C-cc" 'org-capture)
-	      (global-set-key "\C-ca" 'org-agenda)
-	      (global-set-key "\C-cb" 'org-iswitchb)
+  (setq org-capture-templates
+        '(("t" "Todo" entry (file+headline "~/org/notes.org" "Tasks")
+           "* TODO %?\n%T")
+          ("m" "Movie" entry (file+headline "~/org/movie.org" "Movies")
+           "* TODO %?\n%T\n:PROPERTIES:\n:DIRECTOR:\n:STARS:\n:RELEASE DATE:\n:RECOMMENDED BY:\n:END:" )))
 
-	      (setq org-agenda-files '("~/org"))
-	      (setq org-log-into-drawer t)
-	      (setq org-todo-keywords
-		    '((sequence "TODO(t!)" "|" "DONE(d!)")
-		      (sequence "REPORT(r!)" "BUG(b)" "KNOWNCAUSE(k)" "|" "FIXED(f@/!)")
-		      (sequence "|" "CANCELED(c!)")))
-	      (setq org-mobile-directory "~/org/mobileorg")
-	      (setq org-capture-templates
-		    '(("t" "Todo" entry (file+headline "~/org/notes.org" "Tasks")
-		       "* TODO %?\n%T")
-		      ("m" "Movie" entry (file+headline "~/org/movie.org" "Movies")
-		       "* TODO %?\n%T\n:PROPERTIES:\n:DIRECTOR:\n:STARS:\n:RELEASE DATE:\n:RECOMMENDED BY:\n:END:" )))
+  (org-mobile-sync-enable)
 
-	      (setq org-refile-targets '((nil :maxlevel . 2)
-					 (org-agenda-files :maxlevel . 1)))
-
-              (require 'org-crypt)
-              (org-crypt-use-before-save-magic)
-              (setq org-tags-exclude-from-inheritance (quote ("crypt")))
-
-              (setq org-crypt-key nil)
-              ;; GPG key to use for encryption
-              ;; Either the Key ID or set to nil to use symmetric encryption.
-
-              (setq auto-save-default nil)
-              ;; Auto-saving does not cooperate with org-crypt.el: so you need
-              ;; to turn it off if you plan to use org-crypt.el quite often.
-              ;; Otherwise, you'll get an (annoying) message each time you
-              ;; start Org.
-
-              ;; To turn it off only locally, you can insert this:
-              ;;
-              ;; # -*- buffer-auto-save-file-name: nil; -*-
-)
+  (require 'org-crypt)
+  (org-crypt-use-before-save-magic))
