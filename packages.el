4fca031cf278105e4688836672db191156c0ec44
Donghyun Cho <bigeyeguy@gmail.com>
2015-07-08 02:07:58 +0900

    Break lines in package list

--- packages.el
+++ packages.el
@@ -1,3 +1,4 @@
+(require 'cl)
 (require 'package)
 
 ;; Outdated repo
@@ -9,21 +10,74 @@
 
 (package-initialize)
 
-(when (not package-archive-contents)
-  (package-refresh-contents))
-
 (defvar prelude-packages
-  '(eshell erc auto-complete jabber markdown-mode ace-jump-mode
-           ibuffer ido color-theme yasnippet android-mode less-css-mode
-           slim-mode coffee-mode expand-region window-number
-           whitespace linum org iy-go-to-char monokai-theme glsl-mode
-           emacs-eclim projectile flx-ido mvn anaconda-mode
-           company company-anaconda company-c-headers
-           company-go company-irony company-web function-args
-           workgroups2
-           powerline)
+  '(eshell
+    erc
+    auto-complete
+    jabber
+    markdown-mode
+    ace-jump-mode
+    ibuffer
+    ido
+    color-theme
+    yasnippet
+    android-mode
+    less-css-mode
+    slim-mode
+    coffee-mode
+    expand-region
+    window-number
+    whitespace
+    linum
+    org
+    iy-go-to-char
+    monokai-theme
+    glsl-mode
+    emacs-eclim
+    projectile
+    flx-ido
+    mvn
+    anaconda-mode
+    company
+    company-anaconda
+    company-c-headers
+    company-go
+    company-irony
+    company-web
+    function-args
+    workgroups2
+    powerline)
   "A list of packages to ensure are installed at launch.")
 
-(dolist (p prelude-packages)
-  (when (not (package-installed-p p))
-    (package-install p)))
+(defun prelude-packages-installed-p ()
+  "Check if all packages in `prelude-packages' are installed."
+  (every #'package-installed-p prelude-packages))
+
+(defun prelude-require-package (package)
+  "Install PACKAGE unless already installed."
+  (unless (memq package prelude-packages)
+    (add-to-list 'prelude-packages package))
+  (unless (package-installed-p package)
+    (package-install package)))
+
+(defun prelude-require-packages (packages)
+  "Ensure PACKAGES are installed.
+Missing packages are installed automatically."
+  (mapc #'prelude-require-package packages))
+
+
+(define-obsolete-function-alias 'prelude-ensure-module-deps 'prelude-require-packages)
+
+(defun prelude-install-packages ()
+  "Install all packages listed in `prelude-packages'."
+  (unless (prelude-packages-installed-p)
+    ;; check for new packages (package versions)
+    (message "%s" "Emacs Prelude is now refreshing its package database...")
+    (package-refresh-contents)
+
+    (message "%s" " done.")
+    ;; install the missing packages
+    (prelude-require-packages prelude-packages)))
+
+;; run package installation
+(prelude-install-packages)
