e7616e3ef0a5dd6807e38714d27138c9115739a8
Donghyun Cho <bigeyeguy@gmail.com>
2017-09-17 16:56:17 -0700

    Set up java-mode-indent-annotations

--- modules/bigeye-java-mode.el
+++ modules/bigeye-java-mode.el
@@ -1,14 +1,10 @@
-(add-hook 'java-mode-hook
-          (lambda ()
-            "Treat Java 1.5 @-style annotations as comments."
-            (setq c-comment-start-regexp "(@|/(/|[*][*]?))")
-            (modify-syntax-entry ?@ "< b" java-mode-syntax-table)))
+(use-package java-mode-indent-annotations
+  :load-path "vendor/java-mode-indent-annotations")
 
-
-(add-hook 'java-mode-hook (lambda ()
-                            (setq c-basic-offset 4)
-                            (c-set-offset 'arglist-cont-nonempty '++)
-                            (c-set-offset 'statement-cont '++)
-                            ))
+(defun my-java-mode-hook ()
+  (setq c-basic-offset 2)
+  (c-set-offset 'arglist-cont-nonempty '++)
+  (c-set-offset 'statement-cont '++)
+  )
 
 (provide 'bigeye-java-mode)
